da83c68c3aacbcc738e4a68c0ebc3b12e3267e5c
pranjal bhadu <pranjalbhadu171@gmail.com>
2023-09-03 19:38:39 +0530

    added landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header'
+import LandingPage from './pages/LandingPage/LandingPage';
 
 const App = () => (
   <>
     <Header/>
+    <LandingPage/>
     <Footer/>
   </>
 )
--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import './LandingPage.css'
+import { Button, Container, Row } from 'react-bootstrap'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className='main'>
+      <Container>
+        <Row>
+          <div className='intro-text'>
+            <div>
+              <h1 className='title'>Welcome to NoteZipper</h1>
+              <p className='sub-title'>One safe place for all your notes.</p>
+            </div>
+
+            <div className='button-container'>
+              <a href='/login'>
+                <Button size='lg' className='landing-button'>
+                  Login
+                </Button>
+              </a>
+              <a href='/signup'>
+                <Button size='lg' className='landing-button' variant='outline-primary'>
+                  Signup
+                </Button>
+              </a>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
